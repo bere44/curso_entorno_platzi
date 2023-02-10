dad27729b1d7e821e0bc276f8016702bbc8281fc
Berenisse <babitto44@yahoo.com.pe>
2023-02-10 14:09:13 -0500

    aadding files using matloblib

---
+++ Aplicacion/charts.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+def generate_bar_chart(labels,values):
+    fig, ax = plt.subplots()
+    ax.bar(labels,values)
+    plt.show()
+    
+def generate_pie_chart(labels,values):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    ax.axis("equal")
+    plt.show()
+
+if__name__=="__main__":
+    labels = ['A', 'B', 'C']
+    values = [10, 40,800]
+    generate_pie_chart(labels,values)
